459e9cd9a9dd30f34b4e1753c4cb7f73051bebaa
Anders Rognstad <anders.rognstad@nav.no>
2022-11-23 10:26:27 +0100

    api update vurdering

--- src/main/kotlin/no/nav/syfo/aktivitetskrav/api/AktivitetskravVurderingResponseDTO.kt
+++ src/main/kotlin/no/nav/syfo/aktivitetskrav/api/AktivitetskravVurderingResponseDTO.kt
@@ -1,12 +1,13 @@
 package no.nav.syfo.aktivitetskrav.api
 
+import no.nav.syfo.aktivitetskrav.domain.AktivitetskravVurderingStatus
 import java.time.LocalDateTime
 
 data class AktivitetskravVurderingResponseDTO(
     val uuid: String,
     val createdAt: LocalDateTime,
-    val updatedAt: LocalDateTime,
-    val status: String,
+    val sistEndret: LocalDateTime,
+    val status: AktivitetskravVurderingStatus,
     val updatedBy: String?,
     val beskrivelse: String?,
 )
