4f96b5a3582b7151b04c68b99eca719b8c0cffaf
Anders Rognstad <anders.rognstad@nav.no>
2022-12-01 10:07:43 +0100

    Exclude future oppfolgingstilfelle when generating aktivitetskrav
    Include tilfeller starting today

--- src/main/kotlin/no/nav/syfo/util/DateUtil.kt
+++ src/main/kotlin/no/nav/syfo/util/DateUtil.kt
@@ -5,3 +5,5 @@ import java.time.*
 val defaultZoneOffset: ZoneOffset = ZoneOffset.UTC
 
 fun nowUTC(): OffsetDateTime = OffsetDateTime.now(defaultZoneOffset)
+
+fun tomorrow(): LocalDate = LocalDate.now().plusDays(1)
