e7d364dd324f3cb272cf8390080d202de8a76e35
Anders Rognstad <79838644+andersrognstad@users.noreply.github.com>
2023-03-01 14:26:57 +0100

    Read uuids from json and enable in prod (#61)

--- src/main/kotlin/no/nav/syfo/aktivitetskrav/cronjob/AktivitetskravAutomatiskOppfyltCronjob.kt
+++ src/main/kotlin/no/nav/syfo/aktivitetskrav/cronjob/AktivitetskravAutomatiskOppfyltCronjob.kt
@@ -1,13 +1,17 @@
 package no.nav.syfo.aktivitetskrav.cronjob
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import net.logstash.logback.argument.StructuredArguments
 import no.nav.syfo.aktivitetskrav.AktivitetskravService
 import no.nav.syfo.aktivitetskrav.domain.isNy
 import no.nav.syfo.application.cronjob.Cronjob
 import no.nav.syfo.application.cronjob.CronjobResult
 import no.nav.syfo.application.database.DatabaseInterface
+import no.nav.syfo.util.configuredJacksonMapper
 import org.slf4j.LoggerFactory
 import java.util.UUID
+
+private data class UuidListe(val uuids: List<UUID>)
 
 class AktivitetskravAutomatiskOppfyltCronjob(
     private val database: DatabaseInterface,
@@ -15,9 +19,14 @@ class AktivitetskravAutomatiskOppfyltCronjob(
 ) : Cronjob {
     override val initialDelayMinutes: Long = 5
     override val intervalDelayMinutes: Long = 240
+    private val objectMapper: ObjectMapper = configuredJacksonMapper()
+    private val uuids = objectMapper.readValue(
+        this::class.java.getResource("/cronjob/uuids.json")!!.readText(),
+        UuidListe::class.java
+    ).uuids
 
     override suspend fun run() {
-        val result = runJob(uuids.map { UUID.fromString(it) })
+        val result = runJob(uuids)
         log.info(
             "Completed aktivitetskrav automatisk oppfylt processing job with result: {}, {}",
             StructuredArguments.keyValue("failed", result.failed),
@@ -50,7 +59,6 @@ class AktivitetskravAutomatiskOppfyltCronjob(
     }
 
     companion object {
-        private val uuids = listOf<String>()
         private val log = LoggerFactory.getLogger(AktivitetskravAutomatiskOppfyltCronjob::class.java)
     }
 }
