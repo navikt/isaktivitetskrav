157e3cb4401ee06fba5a982c7a1cae5db25a6f7a
Anders Rognstad <79838644+andersrognstad@users.noreply.github.com>
2023-03-03 12:36:30 +0100

    Ikke genererer a-krav dersom vurdert, så gradert, så ikke gradert (#67)

--- src/main/kotlin/no/nav/syfo/aktivitetskrav/domain/Aktivitetskrav.kt
+++ src/main/kotlin/no/nav/syfo/aktivitetskrav/domain/Aktivitetskrav.kt
@@ -116,13 +116,6 @@ fun Aktivitetskrav.isAutomatiskOppfylt(): Boolean =
 
 fun Aktivitetskrav.isNy(): Boolean = this.status == AktivitetskravStatus.NY
 
-fun Aktivitetskrav.isVurdert(): Boolean =
-    this.status in EnumSet.of(
-        AktivitetskravStatus.UNNTAK,
-        AktivitetskravStatus.OPPFYLT,
-        AktivitetskravStatus.IKKE_OPPFYLT,
-    )
-
 fun List<Aktivitetskrav>.toResponseDTOList() = this.map {
     AktivitetskravResponseDTO(
         uuid = it.uuid.toString(),
