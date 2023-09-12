ae47c01f006f8b88e822d244677b89e6257c64da
Anders Rognstad <anders.rognstad@nav.no>
2023-09-12 15:36:53 +0200

    Add DokarkivClient

---
+++ src/main/kotlin/no/nav/syfo/client/dokarkiv/domain/Dokument.kt
@@ -0,0 +1,26 @@
+package no.nav.syfo.client.dokarkiv.domain
+
+enum class BrevkodeType(
+    val value: String,
+) {
+    AKTIVITETSKRAV_FORHANDSVARSEL("OPPF_AKTIVITETSKRAV_FORHANDSVARSEL"),
+}
+
+data class Dokument private constructor(
+    val brevkode: String,
+    val dokumentKategori: String? = null,
+    val dokumentvarianter: List<Dokumentvariant>,
+    val tittel: String? = null,
+) {
+    companion object {
+        fun create(
+            brevkode: BrevkodeType,
+            dokumentvarianter: List<Dokumentvariant>,
+            tittel: String? = null,
+        ) = Dokument(
+            brevkode = brevkode.value,
+            dokumentvarianter = dokumentvarianter,
+            tittel = tittel,
+        )
+    }
+}
---
+++ src/main/kotlin/no/nav/syfo/client/dokarkiv/domain/JournalpostRequest.kt
@@ -0,0 +1,35 @@
+package no.nav.syfo.client.dokarkiv.domain
+
+const val JOURNALFORENDE_ENHET = 9999
+
+enum class JournalpostType(
+    val value: String,
+) {
+    UTGAAENDE("UTGAAENDE"),
+}
+
+enum class JournalpostTema(
+    val value: String,
+) {
+    OPPFOLGING("OPP"),
+}
+
+enum class JournalpostKanal(
+    val value: String,
+) {
+    DITT_NAV("NAV_NO"),
+    SENTRAL_UTSKRIFT("S"),
+}
+
+data class JournalpostRequest(
+    val avsenderMottaker: AvsenderMottaker,
+    val tittel: String,
+    val bruker: Bruker? = null,
+    val dokumenter: List<Dokument>,
+    val journalfoerendeEnhet: Int? = JOURNALFORENDE_ENHET,
+    val journalpostType: String = JournalpostType.UTGAAENDE.value,
+    val tema: String = JournalpostTema.OPPFOLGING.value,
+    val kanal: String,
+    val sak: Sak = Sak(),
+    val overstyrInnsynsregler: String? = null,
+)
