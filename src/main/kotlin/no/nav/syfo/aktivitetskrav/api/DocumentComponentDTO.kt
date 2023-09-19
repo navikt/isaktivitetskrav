ea86c27f752f91baccbea93786740429902ef238
Solgård <vetle.busol@outlook.com>
2023-09-19 14:52:41 +0200

    IS-1605: Refactor

--- src/main/kotlin/no/nav/syfo/aktivitetskrav/api/DocumentComponentDTO.kt
+++ src/main/kotlin/no/nav/syfo/aktivitetskrav/api/DocumentComponentDTO.kt
@@ -7,20 +7,6 @@ data class DocumentComponentDTO(
     val texts: List<String>,
 )
 
-fun List<DocumentComponentDTO>.serialize(): String {
-    return buildString {
-        this@serialize.forEach { documentComponentDTO ->
-            documentComponentDTO.title?.let {
-                appendLine(it)
-            }
-            documentComponentDTO.texts.forEach {
-                appendLine(it)
-            }
-            appendLine()
-        }
-    }
-}
-
 enum class DocumentComponentType {
     HEADER_H1,
     HEADER_H2,
