0f1a8127ef15c6dd93641fc3f5cc7024bf34a7c9
Solgård <vetle.bu.solgard@nav.no>
2023-09-20 13:32:12 +0200

    IS-1605: Fix date formatter

--- src/main/kotlin/no/nav/syfo/client/pdfgen/ForhandsvarselPdfDTO.kt
+++ src/main/kotlin/no/nav/syfo/client/pdfgen/ForhandsvarselPdfDTO.kt
@@ -12,20 +12,18 @@ data class ForhandsvarselPdfDTO private constructor(
     val documentComponents: List<DocumentComponentDTO>,
 ) {
     companion object {
+        val formatter = DateTimeFormatter.ofPattern("dd. MMMM yyyy", Locale("no", "NO"))
+
         fun create(
             mottakerNavn: String,
             mottakerFodselsnummer: String,
             documentComponents: List<DocumentComponentDTO>
         ): ForhandsvarselPdfDTO =
-            ForhandsvarselPdfDTO(mottakerNavn, mottakerFodselsnummer, LocalDate.now().toReadable(), documentComponents)
-
-        /**
-         * Constructs a date on the form 'dd. MMM yyyy' to be displayed as the date the letter was sent in the pdf
-         */
-        private fun LocalDate.toReadable(): String {
-            val formatter = DateTimeFormatter.ofPattern("dd. MMM yyyy", Locale("no", "NO"))
-            val formattedDate = format(formatter)
-            return formattedDate
-        }
+            ForhandsvarselPdfDTO(
+                mottakerNavn,
+                mottakerFodselsnummer,
+                LocalDate.now().format(formatter),
+                documentComponents
+            )
     }
 }
