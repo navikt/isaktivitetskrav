aab13fc7b7c200f656bf64e16b525b6dc67250c1
Eirik Dahlen <37441744+eirikdahlen@users.noreply.github.com>
2023-09-25 10:47:39 +0200

    Add metrics to aktivitetskravvurdering that is published to kafka (#123)

--- src/main/kotlin/no/nav/syfo/aktivitetskrav/kafka/AktivitetskravVurderingProducer.kt
+++ src/main/kotlin/no/nav/syfo/aktivitetskrav/kafka/AktivitetskravVurderingProducer.kt
@@ -1,6 +1,7 @@
 package no.nav.syfo.aktivitetskrav.kafka
 
 import no.nav.syfo.aktivitetskrav.domain.Aktivitetskrav
+import no.nav.syfo.aktivitetskrav.domain.AktivitetskravStatus
 import no.nav.syfo.aktivitetskrav.domain.toKafkaAktivitetskravVurdering
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerRecord
@@ -23,6 +24,20 @@ class AktivitetskravVurderingProducer(
                     kafkaAktivitetskravVurdering,
                 )
             ).get()
+            log.info(
+                "Aktivitetskravvurdering sent with status: {} for uuid: {}",
+                kafkaAktivitetskravVurdering.status,
+                kafkaAktivitetskravVurdering.uuid
+            )
+            when (kafkaAktivitetskravVurdering.status) {
+                AktivitetskravStatus.AVVENT.name -> COUNT_AVVENT.increment()
+                AktivitetskravStatus.UNNTAK.name -> COUNT_UNNTAK.increment()
+                AktivitetskravStatus.OPPFYLT.name -> COUNT_OPPFYLT.increment()
+                AktivitetskravStatus.FORHANDSVARSEL.name -> COUNT_FORHANDSVARSEL.increment()
+                AktivitetskravStatus.STANS.name -> COUNT_STANS.increment()
+                AktivitetskravStatus.IKKE_OPPFYLT.name -> COUNT_IKKE_OPPFYLT.increment()
+                AktivitetskravStatus.IKKE_AKTUELL.name -> COUNT_IKKE_AKTUELL.increment()
+            }
         } catch (e: Exception) {
             log.error(
                 "Exception was thrown when attempting to send KafkaAktivitetskravVurdering with id {}: ${e.message}",
