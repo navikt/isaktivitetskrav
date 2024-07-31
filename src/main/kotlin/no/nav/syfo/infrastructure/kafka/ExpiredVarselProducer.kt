2d13ea6c3673ae17c8d446af98529ec19b92a9b2
Vetle Bu Solgård <vetle.bu.solgard@nav.no>
2024-07-31 13:22:39 +0200

    IS-2431: Update to kotlin 2.0 and jvm 21 (#238)

--- src/main/kotlin/no/nav/syfo/infrastructure/kafka/ExpiredVarselProducer.kt
+++ src/main/kotlin/no/nav/syfo/infrastructure/kafka/ExpiredVarselProducer.kt
@@ -1,26 +1,21 @@
 package no.nav.syfo.infrastructure.kafka
 
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 import no.nav.syfo.infrastructure.kafka.domain.ExpiredVarsel
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.slf4j.LoggerFactory
-import java.util.*
+import java.util.UUID
 
 class ExpiredVarselProducer(private val producer: KafkaProducer<String, ExpiredVarsel>) {
 
-    suspend fun publishExpiredVarsel(expiredVarsel: ExpiredVarsel) {
+    suspend fun publishExpiredVarsel(expiredVarsel: ExpiredVarsel) =
         try {
             val record = ProducerRecord(TOPIC, UUID.randomUUID().toString(), expiredVarsel)
-            withContext(Dispatchers.IO) {
-                producer.send(record).get()
-            }
+            producer.send(record).also { it.get() }
         } catch (e: Exception) {
             log.error("Exception was thrown when attempting to publish expired varsel: ${e.message}")
             throw e
         }
-    }
 
     companion object {
         private const val TOPIC = "teamsykefravr.aktivitetskrav-expired-varsel"
