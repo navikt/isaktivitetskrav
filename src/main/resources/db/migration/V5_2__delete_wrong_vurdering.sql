7db72572b5ea685b7e3ea96f9c5e9aa724e2c3ea
Solgård <vetle.bu.solgard@nav.no>
2024-09-30 15:21:32 +0200

    IS-2692: Delete wrong vurdering

---
+++ src/main/resources/db/migration/V5_2__delete_wrong_vurdering.sql
@@ -0,0 +1,11 @@
+DELETE
+FROM aktivitetskrav
+WHERE uuid = '3321947c-481f-4ac2-8a03-6a7391cbd112';
+
+DELETE
+FROM aktivitetskrav_vurdering
+WHERE uuid = 'ceb892ef-ee8a-455c-bf6d-a75166c9cc53';
+
+DELETE
+FROM aktivitetskrav_vurdering
+WHERE uuid = 'eaf44514-cfb7-41a0-a063-7d9d7b78f46e';
