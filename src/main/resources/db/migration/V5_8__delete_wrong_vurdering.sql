77842098c7752a35f928d59683cdc112ee0719c6
Solgård <vetle.bu.solgard@nav.no>
2025-03-14 11:11:21 +0100

    IS-3172: Delete wrong vurdering

--- src/main/resources/db/migration/V5_8__delete_wrong_vurdering.sql
+++
@@ -1,8 +0,0 @@
-DELETE
-FROM aktivitetskrav_vurdering
-WHERE uuid = '5c88df79-04b1-4214-aa74-c01b8a4e426d';
-
-UPDATE aktivitetskrav
-SET status     = 'NY',
-    updated_at = now()
-where uuid = '67212653-2d39-42ee-9bd0-69dd702b3a3e';
