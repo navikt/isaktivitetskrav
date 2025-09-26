0421a4fc4bb576807ca10b38069ddc34a068e969
Geir Arne Waagbø <79845894+geir-waagboe@users.noreply.github.com>
2025-09-26 14:28:41 +0200

    Update dependency versions and add commons-beanutils
    Updated versions for micrometerRegistry and postgresRuntime. Added new dependency for commons-beanutils with a specific CVE reference.

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,13 +12,13 @@ val kafkaVersion = "3.9.0"
 val ktorVersion = "3.3.0"
 val logbackVersion = "1.5.18"
 val logstashEncoderVersion = "8.1"
-val micrometerRegistryVersion = "1.15.4"
+val micrometerRegistryVersion = "1.12.13"
 val mockkVersion = "1.14.5"
 val nettyCodecVersion = "4.2.6.Final"
 val nimbusJoseJwtVersion = "10.5"
 val postgresVersion = "42.7.8"
 val postgresEmbeddedVersion = "2.1.1"
-val postgresRuntimeVersion = "17.6.0"
+val postgresRuntimeVersion = "17.5.0"
 val redisEmbeddedVersion = "0.7.3"
 
 plugins {
@@ -75,16 +75,16 @@ dependencies {
     }
     implementation("org.apache.kafka:kafka_2.13:$kafkaVersion", excludeLog4j)
     constraints {
-        implementation("org.apache.zookeeper:zookeeper") {
-            because("org.apache.kafka:kafka_2.13:$kafkaVersion -> https://www.cve.org/CVERecord?id=CVE-2023-44981")
-            version {
-                require("3.9.3")
-            }
-        }
         implementation("org.bitbucket.b_c:jose4j") {
             because("org.apache.kafka:kafka_2.13:$kafkaVersion -> https://github.com/advisories/GHSA-6qvw-249j-h44c")
             version {
                 require("0.9.6")
+            }
+        }
+        implementation("commons-beanutils:commons-beanutils") {
+            because("org.apache.kafka:kafka_2.13:$kafkaVersion -> https://www.cve.org/CVERecord?id=CVE-2025-48734")
+            version {
+                require("1.11.0")
             }
         }
     }
@@ -94,7 +94,7 @@ dependencies {
         implementation("org.apache.avro:avro") {
             because("io.confluent:kafka-avro-serializer:$confluentVersion -> https://www.cve.org/CVERecord?id=CVE-2023-39410")
             version {
-                require("1.11.4")
+                require("1.12.0")
             }
         }
         implementation("org.apache.commons:commons-compress") {
