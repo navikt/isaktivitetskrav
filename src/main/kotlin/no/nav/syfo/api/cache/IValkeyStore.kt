3717a5fad01c22f4924e6d8389feb00bd3596607
geir-waagboe <geir.arne.waagbo@nav.no>
2026-07-28 10:20:27 +0200

    IS-4067: Fjerne embedded redis

--- src/main/kotlin/no/nav/syfo/api/cache/IValkeyStore.kt
+++ src/main/kotlin/no/nav/syfo/api/cache/IValkeyStore.kt
@@ -1,7 +1,5 @@
 package no.nav.syfo.api.cache
 
-import no.nav.syfo.util.configuredJacksonMapper
-
 interface IValkeyStore {
     fun get(key: String): String?
     fun set(key: String, value: String, expireSeconds: Long)
